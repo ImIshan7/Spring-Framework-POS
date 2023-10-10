a6bb883baa4b4fa3533794e570eb92e08497955c
ImIshan7 <ishandanu999@gmail.com>
2023-10-10 07:32:33 +0100

    Add Query in Spring

--- O14_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ O14_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @WebAppConfiguration
 @ContextConfiguration(classes = {WebRootConfig.class})
 @ExtendWith(SpringExtension.class)
+@Transactional //data is not save in the database
 class CustomerRepoTest {
 
     @Autowired
@@ -28,5 +30,30 @@ class CustomerRepoTest {
             System.out.println(customer.toString());
         }
     }
+
+
+    @Test
+    public void queryMethodTwo(){
+//        Customer mathara = repo.readByAddress("Mathara");
+//        System.out.println(mathara.toString());
+
+//        Customer mathara = repo.getByAddress("Mathara");
+//        System.out.println(mathara.toString());
+
+//        Customer mathara = repo.queryByAddress("Mathara");
+//        System.out.println(mathara.toString());
+
+//        Long galle = repo.countByAddress("Galle");
+//        System.out.println(galle);
+
+        /*List<Customer> allCustomers = repo.getAllCustomers();
+        for (Customer allCustomer : allCustomers) {
+            System.out.println(allCustomer.toString());
+        }*/
+
+        Customer customer = repo.searchCustomerWithName2("Ishan","Galle");
+        System.out.println(customer);
+    }
+
 
 }
--- O14_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/ItemRepoTest.java
+++ O14_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/ItemRepoTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @WebAppConfiguration
 @ContextConfiguration(classes = {WebRootConfig.class})
 @ExtendWith(SpringExtension.class)
+@Transactional //data is not save in the database
 class ItemRepoTest {
 
     @Autowired
